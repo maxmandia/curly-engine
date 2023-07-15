013adb06709e796c50c84bdf6670cd241231a37a
MaxMandia <mmandia511@gmail.com>
2023-07-15 11:05:32 -0400

    feat: added logic for existing signature

--- app/src/components/EditSignature.tsx
+++ app/src/components/EditSignature.tsx
@@ -70,60 +70,87 @@ function EditSignature(props: EditSignatureProps) {
   }
 
   return (
-    <div className="flex flex-col p-5">
-      <h3 className="text-[20px]">Add a custom signature</h3>
-      <form action="POST" className="flex flex-col mt-2">
-        <label className="text-[14px]" htmlFor="pin">
-          Signature PIN (6 Digits)
-        </label>
-        <input
-          placeholder="Signature PIN"
-          value={userPin}
-          onChange={handlePinChange}
-          className="bg-[#0A0C13] p-1 border-solid border-[#50535E] border-[1px] rounded-[4px] my-3 text-[#D1D4DC]"
-          type="number"
-        />
-        <label className="text-[14px]" htmlFor="font">
-          Signature Font Style
-        </label>
-        <select
-          placeholder="Cedarville Cursive"
-          className="bg-[#0A0C13] p-1 border-solid border-[#50535E] border-[1px] rounded-[4px] my-3 text-[#D1D4DC]"
-          name="font"
-          id=""
-          onChange={(e) => setSelectedFont(e.target.value)}
-        >
-          <option value="Cedarville Cursive">Cedarville Cursive</option>
-          <option value="Great Vibes">Great Vibes</option>
-          <option value="Tangerine">Tangerine</option>
-        </select>
-      </form>
-      <h5 className="text-[14px]">Signature Preview</h5>
-      <div className="bg-[#D2D2D2] rounded-[6px] py-5 flex justify-center mt-3 text-black">
-        <p
-          className="text-[48px]"
-          style={{
-            fontFamily: selectedFont,
-          }}
-        >
-          {selectedUser.name}
-        </p>
-      </div>
-      <div className="flex items-center justify-end gap-6 mt-5">
-        <button
-          onClick={() => setIsEditing(false)}
-          className="text-ritten-blue"
-        >
-          Cancel
-        </button>
-        <button
-          onClick={handleSave}
-          className="bg-ritten-blue px-4 py-2 rounded-[6px] text-black shadow-user-shadow"
-        >
-          Add Signature
-        </button>
-      </div>
-    </div>
+    <>
+      {selectedUser.signature ? (
+        <div className="flex flex-col p-5">
+          <h5 className="text-[14px]">Signature Preview</h5>
+          <div className="bg-[#D2D2D2] rounded-[6px] py-5 flex justify-center mt-3 text-black">
+            <p
+              className="text-[48px]"
+              style={{
+                fontFamily: selectedUser.signature.fontStyle,
+              }}
+            >
+              {selectedUser.name}
+            </p>
+          </div>
+          <div className="flex items-center justify-end gap-6 mt-5">
+            <button
+              onClick={() => setIsEditing(false)}
+              className="bg-ritten-blue px-4 py-2 rounded-[6px] text-black shadow-user-shadow"
+            >
+              Go Back
+            </button>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col p-5">
+          <h3 className="text-[20px]">Add a custom signature</h3>
+          <form action="POST" className="flex flex-col mt-2">
+            <label className="text-[14px]" htmlFor="pin">
+              Signature PIN (6 Digits)
+            </label>
+            <input
+              placeholder="Signature PIN"
+              value={userPin}
+              onChange={handlePinChange}
+              className="bg-[#0A0C13] p-1 border-solid border-[#50535E] border-[1px] rounded-[4px] my-3 text-[#D1D4DC]"
+              type="number"
+            />
+            <label className="text-[14px]" htmlFor="font">
+              Signature Font Style
+            </label>
+            <select
+              placeholder="Cedarville Cursive"
+              className="bg-[#0A0C13] p-1 border-solid border-[#50535E] border-[1px] rounded-[4px] my-3 text-[#D1D4DC]"
+              name="font"
+              id=""
+              onChange={(e) => setSelectedFont(e.target.value)}
+            >
+              <option value="Cedarville Cursive">Cedarville Cursive</option>
+              <option value="Great Vibes">Great Vibes</option>
+              <option value="Tangerine">Tangerine</option>
+              <option value="Dancing Script">Dancing Scrip</option>
+            </select>
+          </form>
+          <h5 className="text-[14px]">Signature Preview</h5>
+          <div className="bg-[#D2D2D2] rounded-[6px] py-5 flex justify-center mt-3 text-black">
+            <p
+              className="text-[48px]"
+              style={{
+                fontFamily: selectedFont,
+              }}
+            >
+              {selectedUser.name}
+            </p>
+          </div>
+          <div className="flex items-center justify-end gap-6 mt-5">
+            <button
+              onClick={() => setIsEditing(false)}
+              className="text-ritten-blue"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={handleSave}
+              className="bg-ritten-blue px-4 py-2 rounded-[6px] text-black shadow-user-shadow"
+            >
+              Add Signature
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
